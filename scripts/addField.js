3364d4ea40f65f285c60c80f1886adb3101e63a1
lenamiroux <helena.simoes@me.com>
2020-08-05 13:40:43 -0300

    Creating Script to add new schedule fields

---
+++ scripts/addField.js
@@ -0,0 +1,16 @@
+// Procurar o botão
+document.querySelector('#add-time').addEventListener('click', cloneField);
+
+function cloneField() {
+  const newFieldContainer = document
+    .querySelector('.schedule-item')
+    .cloneNode(true);
+
+  const fields = newFieldContainer.querySelectorAll('input');
+
+  fields.forEach(function (field) {
+    field.value = '';
+  });
+
+  document.querySelector('#schedule-items').appendChild(newFieldContainer);
+}
